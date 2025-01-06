2ca391bf750b55fb5f05b02fec569457e12695a8
Marco Chinello <marco.chinello@gmail.com>
2025-01-06 18:37:54 +0000

    update template

--- tests/test_ApplicationGenerator.py
+++ tests/test_ApplicationGenerator.py
@@ -1,8 +1,6 @@
 from modules.ApplicationGenerator import ApplicationGenerator
 from modules.Expose import Expose
 
-
-
 def main():
     print("Testing Application Generator")
     expose = Expose(expose_id="00000", source = "Immobilienscout24", location = "Berlin, Tempelhof", agent_name = "Herr Piffer")
